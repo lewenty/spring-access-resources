4d22971d4932dd0c2347fcd7346376a0ab8b71bb
levent.yildiz <levent.yildiz@turkkep.com.tr>
2014-08-24 23:13:03 +0300

    clean and reformat code

--- src/main/java/com/company/controller/DefaultController.java
+++ src/main/java/com/company/controller/DefaultController.java
@@ -13,14 +13,11 @@ public class DefaultController {
     @Value("${db.url}")
     public String dbUrl;
 
-
     @Value("${db.user}")
     public String dbUser;
 
-
     @Value("${db.pass}")
     public String dbPass;
-
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
@@ -29,7 +26,6 @@ public class DefaultController {
         model.addAttribute("url", this.dbUrl); // url bilgisini ekle
         model.addAttribute("user", this.dbUser); // user bilgisini ekle
         model.addAttribute("pass", this.dbPass); // pass bilgisini ekle
-
 
         return "index";
     }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,15 +1,11 @@
 <html>
 <body>
 <h5>${message}</h5>
-
 <hr/>
 <b> URL : </b> ${url}
-
 <hr/>
 <b> Username : </b> ${user}
-
 <hr/>
 <b> Password : </b> ${pass}
-
 </body>
 </html>
